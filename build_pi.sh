62e0ffe0aa683b8f01ebbf87b38a22e75eeacc0b
gurux13 <gurux13@gmail.com>
2026-02-01 19:54:29 +0000

    Initial build script

---
+++ build_pi.sh
@@ -0,0 +1,8 @@
+apt install -y libwiringpi-dev gcc g++ make wget libpaho-mqtt-dev
+wget https://github.com/WiringPi/WiringPi/releases/download/3.16/wiringpi_3.16_arm64.deb
+apt install ./wiringpi_3.16_arm64.deb
+cd -- $(dirname "$0")
+cd RaspberryPi/
+make
+ln -s /lib/libwiringPi.so /lib/libwiringPi.so.2
+cp SmartSwitch /bin
