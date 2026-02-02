e256b370cc5cfa7e8ac6fb3f00932ba05675920b
gurux13 <gurux13@gmail.com>
2026-02-02 00:50:52 +0000

    Debugging

--- build_pi.sh
+++ build_pi.sh
@@ -1,7 +1,12 @@
+#!/bin/sh
+set -e
 apt install -y gcc g++ make wget libpaho-mqtt-dev gdb
 wget https://github.com/WiringPi/WiringPi/releases/download/3.16/wiringpi_3.16_arm64.deb
 apt install ./wiringpi_3.16_arm64.deb
 cd -- $(dirname "$0")
+cd rc-switch/
+patch -p1 < ../rc-switch.diff
+cd ../
 cd RaspberryPi/
 make
 ln -s /lib/libwiringPi.so /lib/libwiringPi.so.2
